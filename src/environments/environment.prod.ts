a998d9c495b3c88fe6872384a2b4e7b18fbc28ab
tineapedis <nmurata73@gmail.com>
2021-03-07 13:52:23 +0900

    modify environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCSdq-Fp9H4yS6voqnqIwt-sRdQ14C-3FQ',
+    authDomain: 'todo-demo-app-anguar.firebaseapp.com',
+    databaseURL: 'https://todo-demo-app-anguar.firebaseio.com',
+    projectId: 'todo-demo-app-anguar',
+    storageBucket: 'todo-demo-app-anguar.appspot.com',
+    // messagingSenderId: '<your-messaging-sender-id>',
+    appId: '1:416655667895:web:f8746c31bcc6acd0ca5332'
+    // measurementId: 'G-'
+  }
 };
